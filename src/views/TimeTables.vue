590b71d3ec8f18de09e35987ce437f559567c7b8
Greydius <mr.greydius@gmail.com>
2022-01-08 10:33:53 +0500

    last

--- src/views/TimeTables.vue
+++ src/views/TimeTables.vue
@@ -24,7 +24,7 @@
                 <td>{{ tb.start_at }}</td>
                 <td>{{ tb.bracket.first_fighter && `${tb.bracket.first_fighter.city} "${tb.bracket.first_fighter.club}" -  ${tb.bracket.first_fighter.name}` }}</td>
                 <td>vs</td>
-                <td>{{ tb.bracket.second_fighter && `${tb.bracket.second_fighter.city} "${tb.bracket.first_fighter.club}" -  ${tb.bracket.second_fighter.name}` }}</td>
+                <td>{{ tb.bracket.second_fighter && `${tb.bracket.second_fighter.city} "${tb.bracket.second_fighter.club}" -  ${tb.bracket.second_fighter.name}` }}</td>
               </tr>  
             </template>
             
